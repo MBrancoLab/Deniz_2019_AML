cf0b30c0c7da733740327169754e28b22f527bc2
Branco Lab <33059644+MBrancoLab@users.noreply.github.com>
2019-11-15 11:11:34 +0000

    Update ChIPseq_trends.sh

--- Shell_scripts/ChIPseq_trends.sh
+++ Shell_scripts/ChIPseq_trends.sh
@@ -7,11 +7,15 @@
 module load use.own
 module load homer
 
+##get name of bigwig file to process (not included in this repository)
 bw=$(sed -n "${SGE_TASK_ID}p" bw_list.txt)
 
+##convert to bedGraph
 bedgraph=$(echo $bw | sed s/bw$/bedGraph/)
 ./bigWigToBedGraph $bw $bedgraph
 
+##generate data to draw trend plots across LTR2B elements (+/- 1.5kb from centre) belonging to different clusters
+##clusters defined in define_clusters.R and respective annotation in Annotations folder
 for k in {1..7}; do
-	annotatePeaks.pl LTR2B_cluster${k}_3kb.bed hg38 -size 3000 -hist 100 -bedGraph $bedgraph > LTR2B_cluster${k}_${bedgraph}_trend.txt
+	annotatePeaks.pl ../Annotations/LTR2B_cluster${k}_3kb.bed hg38 -size 3000 -hist 100 -bedGraph $bedgraph > LTR2B_cluster${k}_${bedgraph}_trend.txt
 done
