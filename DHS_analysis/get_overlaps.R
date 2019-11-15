5658ff1884c8a1adf139acef7ee1a4b609f5832b
Branco Lab <33059644+MBrancoLab@users.noreply.github.com>
2019-11-15 06:45:22 +0000

    Update get_overlaps.R

--- DHS_analysis/get_overlaps.R
+++ DHS_analysis/get_overlaps.R
@@ -1,4 +1,8 @@
-setwd('~/Desktop/AML/AML_scripts/DHS_analysis')
+##Generates a matrix of DHS-TE overlaps for the selected TE families
+##Uses bedtools
+
+setwd('~/Deniz_2019_AML/DHS_analysis')
+path_to_intersectbed = "~/Documents/bedtools-2.26.0/bin/intersectBed"
 
 
 ##intersect function
@@ -6,7 +10,7 @@ setwd('~/Desktop/AML/AML_scripts/DHS_analysis')
 intersectBED <- function(a.file,b.file,opt.string="-c") {
 	out=tempfile()
 	
-	command=paste("~/Documents/bedtools-2.26.0/bin/intersectBed -a", a.file,"-b",b.file,opt.string,">",out,sep=" ")
+	command=paste(path_to_intersectbed,"-a", a.file,"-b",b.file,opt.string,">",out,sep=" ")
 	cat(command,"\n")
 	try(system(command))
 	res=read.table(out,header=F,as.is=T)
@@ -23,7 +27,7 @@ dhs.files = dhs.files[!grepl('Macrophages.C005VG45',dhs.files)] #remove macropha
 
 fnames = unlist(lapply(strsplit(dhs.files,split='/'),function(x) x[length(x)]))
 
-ltr.file = '../Annotations/all_LTRs.bed'
+ltr.file = '../Annotations/all_LTRs.bed'  #Repeatmasker annotations for LTR2B, LTR2C, LTR5B, LTR5_Hs, LTR12C, LTR13A
 
 
 ##overlap
