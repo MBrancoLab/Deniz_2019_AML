84da3736bb96cd9be8e876489c65d47a064122bf
Branco Lab <33059644+MBrancoLab@users.noreply.github.com>
2019-11-15 17:47:19 +0000

    Update fimo.sh

--- Shell_scripts/fimo.sh
+++ Shell_scripts/fimo.sh
@@ -2,7 +2,8 @@
 ##Uses FIMO tool from MEME and the HOCOMOCO v11 database
 ##Input are fasta files separated by family
 
+database=~/Documents/motif_databases/HUMAN/HOCOMOCOv11_core_HUMAN_mono_meme_format.meme
 
 for te in LTR2B LTR2C LTR5B LTR5_Hs LTR12C LTR13A; do
-	fimo --o ${te}_fimo ~/Documents/motif_databases/HUMAN/HOCOMOCOv11_core_HUMAN_mono_meme_format.meme ${te}_dhs.fa 
+	fimo --o ${te}_fimo $database ../TF_motifs/fasta_files/${te}_dhs.fa 
 done
