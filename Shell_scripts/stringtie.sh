9609ef8896d276c88c4f69603e768255494ed977
Branco Lab <33059644+MBrancoLab@users.noreply.github.com>
2019-11-15 07:53:31 +0000

    Update stringtie.sh

--- Shell_scripts/stringtie.sh
+++ Shell_scripts/stringtie.sh
@@ -5,7 +5,9 @@
 #$ -l h_vmem=4G
 #$ -t 1-5
 
+##get bam file names from STAR alignments (not included in this repository)
 bam=$(sed -n "${SGE_TASK_ID}p" bam_list.txt)
 out=$(echo $bam | sed s/.bam/.gtf/)
 
+##run Stringtie guided by a Gencode annotation
 ~/stringtie/stringtie $bam —rf -G ~/scratch/Genomes/Human/GRCh38/GRCh38_gencode_v26_CTAT_lib_July192017/ctat_genome_lib_build_dir/ref_annot.gtf -o $out
