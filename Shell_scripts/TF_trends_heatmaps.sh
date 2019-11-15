432b3d83520179b87adde27f62eb3343c00694ef
Branco Lab <33059644+MBrancoLab@users.noreply.github.com>
2019-11-15 13:35:40 +0000

    Update TF_trends_heatmaps.sh

--- Shell_scripts/TF_trends_heatmaps.sh
+++ Shell_scripts/TF_trends_heatmaps.sh
@@ -2,35 +2,36 @@
 #$ -cwd
 #$ -V
 #$ -l h_vmem=4G
-#$ -m bea
 
 module load use.own
 module load homer
 
+
+#Each line in set_lists.txt corresponds to a file that lists the ChIP and control datasets for a given TF (K562_TF_sets.zip in this repository)
 
 while read file; do
 
 	tf=$(echo $file | sed s/_sets.txt//)
 
 
-	##trend plot
+	##generate trend plots (not used in the paper)
 
 	while read set; do
-		makeTagDirectory $set-tags/ $set-hg38.sam.gz -format sam -keepAll
+		makeTagDirectory $set-tags/ $set-hg38.sam.gz -format sam -keepAll #sam files not included in repository
 	done < $file
 
 	for te in LTR2B LTR2C LTR5B LTR5_Hs LTR12C LTR13A; do
-		annotatePeaks.pl ${te}_hg38_3kb.bed hg38 -size 3000 -hist 10 -d *-tags/ > $te-$tf-trend.txt
+		annotatePeaks.pl ../Annotations/${te}_hg38_3kb.bed hg38 -size 3000 -hist 10 -d *-tags/ > $te-$tf-trend.txt
 	done
 
 
-	##heatmap
+	##generate profile heatmaps
 
 	while read set;	do
 		rm -r $set-tags
 		makeTagDirectory $set-tags/ $set-hg38.sam.gz -format sam -unique
 		for te in LTR2B LTR2C LTR5B LTR5_Hs LTR12C LTR13A; do
-			annotatePeaks.pl ${te}_hg38_3kb.bed hg38 -size 3000 -hist 10 -ghist -d $set-tags/ > $te-$set-heatmap.txt
+			annotatePeaks.pl ../Annotations/${te}_hg38_3kb.bed hg38 -size 3000 -hist 10 -ghist -d $set-tags/ > $te-$set-heatmap.txt
 		done
 		rm -r $set-tags
 	done < $file
